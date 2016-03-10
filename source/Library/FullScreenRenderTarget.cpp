9bd8ed2dfa011e01982ee294c7c0d220e1ff8ae8
Albeforia <cnntcyj@126.com>
2016-03-11 05:17:00 +0800

    add format option

--- source/Library/FullScreenRenderTarget.cpp
+++ source/Library/FullScreenRenderTarget.cpp
@@ -4,15 +4,16 @@
 
 namespace Library {
 
-	FullScreenRenderTarget::FullScreenRenderTarget(Game& game)
-		: m_game {&game}, m_render_target_view {}, m_depth_stencil_view {}, m_output_texture {} {
+	FullScreenRenderTarget::FullScreenRenderTarget(Game& game,
+												   ID3D11DepthStencilView* depth_stencil, DXGI_FORMAT format)
+		: m_game {&game}, m_render_target {}, m_depth_stencil {depth_stencil}, m_output_texture {} {
 		D3D11_TEXTURE2D_DESC desc;
 		ZeroMemory(&desc, sizeof(desc));
 		desc.Width = game.screen_width();
 		desc.Height = game.screen_height();
 		desc.MipLevels = 1;
 		desc.ArraySize = 1;
-		desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desc.Format = format;
 		desc.SampleDesc.Count = 1;
 		desc.SampleDesc.Quality = 0;
 		desc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
@@ -28,58 +29,52 @@ namespace Library {
 			ReleaseObject(tex);
 			throw GameException("IDXGIDevice::CreateShaderResourceView() failed.", hr);
 		}
-		if (FAILED(hr = dev->CreateRenderTargetView(tex, nullptr, &m_render_target_view))) {
+		if (FAILED(hr = dev->CreateRenderTargetView(tex, nullptr, &m_render_target))) {
 			ReleaseObject(tex);
 			throw GameException("IDXGIDevice::CreateRenderTargetView() failed.", hr);
 		}
 		ReleaseObject(tex);
 
-		ZeroMemory(&desc, sizeof(desc));
-		desc.Width = game.screen_width();
-		desc.Height = game.screen_height();
-		desc.MipLevels = 1;
-		desc.ArraySize = 1;
-		desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-		desc.SampleDesc.Count = 1;
-		desc.SampleDesc.Quality = 0;
-		desc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
-		desc.Usage = D3D11_USAGE_DEFAULT;
+		if (m_depth_stencil == nullptr) {
+			ZeroMemory(&desc, sizeof(desc));
+			desc.Width = game.screen_width();
+			desc.Height = game.screen_height();
+			desc.MipLevels = 1;
+			desc.ArraySize = 1;
+			desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+			desc.SampleDesc.Count = 1;
+			desc.SampleDesc.Quality = 0;
+			desc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+			desc.Usage = D3D11_USAGE_DEFAULT;
 
-		ID3D11Texture2D* tex1 = nullptr;
-		if (FAILED(hr = dev->CreateTexture2D(&desc, nullptr, &tex1))) {
-			throw GameException("IDXGIDevice::CreateTexture2D() failed.", hr);
-		}
-		if (FAILED(hr = dev->CreateDepthStencilView(tex1, nullptr, &m_depth_stencil_view))) {
+			ID3D11Texture2D* tex1 = nullptr;
+			if (FAILED(hr = dev->CreateTexture2D(&desc, nullptr, &tex1))) {
+				throw GameException("IDXGIDevice::CreateTexture2D() failed.", hr);
+			}
+			if (FAILED(hr = dev->CreateDepthStencilView(tex1, nullptr, &m_depth_stencil))) {
+				ReleaseObject(tex1);
+				throw GameException("IDXGIDevice::CreateDepthStencilView() failed.", hr);
+			}
 			ReleaseObject(tex1);
-			throw GameException("IDXGIDevice::CreateDepthStencilView() failed.", hr);
 		}
-		ReleaseObject(tex1);
 	}
 
 	FullScreenRenderTarget::~FullScreenRenderTarget() {
 		ReleaseObject(m_output_texture);
-		ReleaseObject(m_depth_stencil_view);
-		ReleaseObject(m_render_target_view);
+		ReleaseObject(m_depth_stencil);
+		ReleaseObject(m_render_target);
 	}
 
 	ID3D11ShaderResourceView* FullScreenRenderTarget::output_texture() const {
 		return m_output_texture;
 	}
 
-	ID3D11RenderTargetView* FullScreenRenderTarget::render_target_view() const {
-		return m_render_target_view;
+	ID3D11RenderTargetView* FullScreenRenderTarget::render_target() const {
+		return m_render_target;
 	}
 
-	ID3D11DepthStencilView* FullScreenRenderTarget::depth_stencil_view() const {
-		return m_depth_stencil_view;
-	}
-
-	void FullScreenRenderTarget::begin() {
-		m_game->d3d_device_context()->OMSetRenderTargets(1, &m_render_target_view, m_depth_stencil_view);
-	}
-
-	void FullScreenRenderTarget::end() {
-		m_game->reset_render_targets();
+	ID3D11DepthStencilView* FullScreenRenderTarget::depth_stencil() const {
+		return m_depth_stencil;
 	}
 
 }
--- source/Library/FullScreenRenderTarget.h
+++ source/Library/FullScreenRenderTarget.h
@@ -8,20 +8,18 @@ namespace Library {
 
 	class FullScreenRenderTarget {
 	public:
-		FullScreenRenderTarget(Game&);
+		FullScreenRenderTarget(Game&,
+							   ID3D11DepthStencilView* ds = nullptr, DXGI_FORMAT format = DXGI_FORMAT_R8G8B8A8_UNORM);
 		~FullScreenRenderTarget();
 
+		ID3D11RenderTargetView* render_target() const;
+		ID3D11DepthStencilView* depth_stencil() const;
 		ID3D11ShaderResourceView* output_texture() const;
-		ID3D11RenderTargetView* render_target_view() const;
-		ID3D11DepthStencilView* depth_stencil_view() const;
-
-		void begin();
-		void end();
 
 	private:
 		Game* m_game;
-		ID3D11RenderTargetView* m_render_target_view;
-		ID3D11DepthStencilView* m_depth_stencil_view;
+		ID3D11RenderTargetView* m_render_target;
+		ID3D11DepthStencilView* m_depth_stencil;
 		ID3D11ShaderResourceView* m_output_texture;
 	};
 
