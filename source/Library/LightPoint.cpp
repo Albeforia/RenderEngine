57cb8c933eb16ae84b54629722f1230fd27e6c46
Albeforia <cnntcyj@126.com>
2016-03-12 02:14:52 +0800

    add LightPoint

---
+++ source/Library/LightPoint.cpp
@@ -0,0 +1,74 @@
+#include "LightPoint.h"
+#include "VectorHelper.h"
+
+namespace Library {
+
+	RTTI_DEFINITIONS(LightPoint);
+
+	LightPoint::LightPoint(Game& game)
+		: Light(game), m_position {Vector3Helper::Zero}, m_radius {1.0f} {}
+
+	LightPoint::~LightPoint() {}
+
+	XMFLOAT3& LightPoint::position() {
+		return m_position;
+	}
+
+	XMVECTOR LightPoint::positionv() const {
+		return XMLoadFloat3(&m_position);
+	}
+
+	float LightPoint::radius() const {
+		return m_radius;
+	}
+
+	void LightPoint::set_position(float x, float y, float z) {
+		XMVECTOR position = XMVectorSet(x, y, z, 1.0f);
+		set_position(position);
+	}
+
+	void LightPoint::set_position(FXMVECTOR position) {
+		XMStoreFloat3(&m_position, position);
+	}
+
+	void LightPoint::set_position(const XMFLOAT3& position) {
+		m_position = position;
+	}
+
+	void LightPoint::set_attenuation(float constant, float linear, float quadratic) {
+		m_constant = constant;
+		m_linear = linear;
+		m_quadratic = quadratic;
+		update_radius();
+	}
+
+	void LightPoint::set_color(FLOAT r, FLOAT g, FLOAT b, FLOAT a) {
+		Light::set_color(r, g, b, a);
+		update_radius();
+	}
+	void LightPoint::set_color(XMCOLOR c) {
+		Light::set_color(c);
+		update_radius();
+	}
+	void LightPoint::set_color(FXMVECTOR c) {
+		Light::set_color(c);
+		update_radius();
+	}
+	void LightPoint::set_intensity(float i) {
+		Light::set_intensity(i);
+		update_radius();
+	}
+
+	void LightPoint::update_radius() {
+		XMVECTOR v = colorv();
+		float r = XMVectorGetX(v);
+		float g = XMVectorGetY(v);
+		float b = XMVectorGetZ(v);
+		float max = std::fmaxf(std::fmaxf(r, g), b);
+		m_radius = (-m_linear +
+					std::sqrtf(m_linear * m_linear -
+							   4 * m_quadratic * (m_constant - 256.0 * intensity() * max)))
+			/ (2 * m_quadratic);
+	}
+
+}
---
+++ source/Library/LightPoint.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Common.h"
+#include "Light.h"
+
+namespace Library {
+
+	class LightPoint : public Light {
+
+		RTTI_DECLARATIONS(LightPoint, Light)
+
+	public:
+		LightPoint(Game&);
+		virtual ~LightPoint();
+
+		XMFLOAT3& position();
+		XMVECTOR positionv() const;
+		float radius() const;
+
+		virtual void set_position(float x, float y, float z);
+		virtual void set_position(FXMVECTOR);
+		virtual void set_position(const XMFLOAT3&);
+		virtual void set_attenuation(float constant, float linear, float quadratic);
+
+		virtual void set_color(FLOAT r, FLOAT g, FLOAT b, FLOAT a) override;
+		virtual void set_color(XMCOLOR) override;
+		virtual void set_color(FXMVECTOR) override;
+		virtual void set_intensity(float) override;
+
+	protected:
+		void update_radius();
+
+		XMFLOAT3 m_position;
+		float m_radius;
+		float m_constant;
+		float m_linear;
+		float m_quadratic;
+
+	};
+
+}
