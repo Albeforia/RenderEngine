1e145aac1e8e60e6d4f1987edda1589a21a2b81f
Albeforia <cnntcyj@126.com>
2016-03-11 17:35:57 +0800

    remove vertex color

--- source/Library/MaterialBasic.cpp
+++ source/Library/MaterialBasic.cpp
@@ -29,27 +29,12 @@ namespace Library {
 		assert(tex_coords->size() == src_vertices.size());
 		std::vector<VertexMaterialBasic> vertices;
 		vertices.reserve(src_vertices.size());
-		if (mesh.vertex_colors().size() > 0) {
-			auto* vertex_colors = mesh.vertex_colors().at(0);
-			assert(vertex_colors->size() == src_vertices.size());
-			for (UINT i = 0; i < src_vertices.size(); i++) {
-				XMFLOAT3 pos = src_vertices.at(i);
-				XMFLOAT4 color = vertex_colors->at(i);
-				XMFLOAT3 uv = tex_coords->at(i);
-				vertices.push_back(VertexMaterialBasic {
-					XMFLOAT4(pos.x, pos.y, pos.z, 1.0f), XMFLOAT2(uv.x, uv.y)
-				});
-			}
-		}
-		else {
-			XMFLOAT4 color = XMFLOAT4(reinterpret_cast<const float*>(&ColorHelper::White));
-			for (UINT i = 0; i < src_vertices.size(); i++) {
-				XMFLOAT3 pos = src_vertices.at(i);
-				XMFLOAT3 uv = tex_coords->at(i);
-				vertices.push_back(VertexMaterialBasic {
-					XMFLOAT4(pos.x, pos.y, pos.z, 1.0f), XMFLOAT2(uv.x, uv.y)
-				});
-			}
+		for (UINT i = 0; i < src_vertices.size(); i++) {
+			XMFLOAT3 pos = src_vertices.at(i);
+			XMFLOAT3 uv = tex_coords->at(i);
+			vertices.push_back(VertexMaterialBasic {
+				XMFLOAT4(pos.x, pos.y, pos.z, 1.0f), XMFLOAT2(uv.x, uv.y)
+			});
 		}
 		Material::create_vertex_buffer(device, reinterpret_cast<void*>(&vertices[0]), vertices.size(), buffer);
 	}
